094b39aae3ecb3773609d80c21a17ec07be2aae7
Fiera Ziadattun Nisa <fieraziadattunnisa@gmail.com>
2026-02-24 11:59:59 +0700

    first commit

---
+++ praktikum/my-app/src/pages/about.js
@@ -0,0 +1,10 @@
+export default function About() {
+    return (
+        <div>
+            <h1>Praktikum Next.js Pages Router</h1>
+            <p>Nama : Fiera Ziadattun Nisa'</p>
+            <p>NIM : 2341720220</p>
+            <p>Program Studi : D4 Teknik Informatika</p>
+        </div>
+    )
+}
